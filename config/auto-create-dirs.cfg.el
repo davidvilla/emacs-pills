3a1248ea988e09f5ed2881f073055ac21472386d
David Villa Alises <David.Villa@gmail.com>
2013-03-14 18:59:46 +0100

    auto-create-dirs
    --HG--
    rename : debian/arco-emacs-python.install => debian/emacs-pills-python.install
    rename : debian/arco-emacs.install => debian/emacs-pills.install

---
+++ config/auto-create-dirs.cfg.el
@@ -0,0 +1,13 @@
+; -*- mode:lisp -*-
+
+;| Create intermediate directories when saving a new file
+
+; http://stackoverflow.com/questions/6830671/how-to-make-emacs-create-intermediate-dirs-when-saving-a-file
+
+(add-hook 'before-save-hook
+  (lambda ()
+    (when buffer-file-name
+      (let ((dir (file-name-directory buffer-file-name)))
+        (when (and (not (file-exists-p dir))
+                   (y-or-n-p (format "Directory %s does not exist. Create it? " dir)))
+          (make-directory dir t))))))
