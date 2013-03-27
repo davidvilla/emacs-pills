001a1e165b324def230554c1cad48c25966d250a
David Villa Alises <David.Villa@gmail.com>
2013-03-27 15:15:52 +0100

    tunning

--- config/compile.cfg.el
+++ config/compile.cfg.el
@@ -15,15 +15,10 @@
 
 
 ; http://lazywithclass.posterous.com/emacs-functions-to-ease-tdd
-(defun color-modeline(exit-value)
+(defun color-modeline(color)
   "Colors the modeline, green success red failure"
   (interactive)
-  (let (test-result-color)
-    (if (= exit-value 0)
-        ((setq test-result-color "Green")
-	 (run-at-time "2 sec" nil 'no-color-modeline))
-      (setq test-result-color "Red"))
-    (set-face-background 'modeline test-result-color)))
+  (set-face-background 'modeline color))
 
 (defun no-color-modeline()
   "No color for the modeline"
@@ -32,32 +27,69 @@
 
 
 ; from http://emacswiki.org/emacs/CompileCommand (Xu Yuan)
-(defun notify-compilation-result(buffer msg)
-  "Notify that the compilation is finished,
-close the *compilation* buffer if the compilation is successful,
-and set the focus back to Emacs frame"
-  (if (string-match "^finished" msg)
+; (defun notify-compilation-result(buffer msg)
+;   "Notify that the compilation is finished,
+; close the *compilation* buffer if the compilation is successful,
+; and set the focus back to Emacs frame"
+;   (if (string-match "^finished" msg)
+;     (progn
+;      (delete-windows-on buffer)
+;      (color-modeline 0))
+;     (color-modeline 1))
+;   (setq current-frame (car (car (cdr (current-frame-configuration)))))
+;   (select-frame-set-input-focus current-frame)
+;   )
+;
+; (add-to-list 'compilation-finish-functions 'notify-compilation-result)
+
+
+(defun compilation-exit-hook (status code msg)
+  ;; If M-x compile exists with a 0
+  (if (and (eq status 'exit) (zerop code))
     (progn
-;     (delete-windows-on buffer)
-     (color-modeline 0))
-    (color-modeline 1))
+      (if (string-match "warning:" (buffer-string))
+	  (color-modeline "orange")
+	  (progn (color-modeline "YellowGreen")
+	   (run-at-time "2 sec" nil 'no-color-modeline)))
+      (delete-windows-on (get-buffer "*compilation*"))
+      )
+    (color-modeline "OrangeRed"))
   (setq current-frame (car (car (cdr (current-frame-configuration)))))
   (select-frame-set-input-focus current-frame)
-  )
+  ;; Always return the anticipated result of compilation-exit-message-function
+  (cons msg code))
+;
+(setq compilation-exit-message-function 'compilation-exit-hook)
 
 
-(add-to-list 'compilation-finish-functions 'notify-compilation-result)
+; ;; Bury the compilation buffer when compilation is finished and successful.
+; (add-to-list 'compilation-finish-functions
+;   (lambda (buffer msg)
+;     (when (bury-buffer buffer)
+;       (replace-buffer-in-windows buffer))))
 
 ;http://vwood.github.com/emacs-compile-on-save.html
 ;; Prevent compilation buffer from showing up
-(defadvice compile (around compile/save-window-excursion first () activate)
-  (save-window-excursion ad-do-it))
+;(defadvice compile (around compile/save-window-excursion first () activate)
+;  (save-window-excursion ad-do-it))
 
-;; Bury the compilation buffer when compilation is finished and successful.
-(add-to-list 'compilation-finish-functions
-             (lambda (buffer msg)
-               (when
-                 (bury-buffer buffer)
-                 (replace-buffer-in-windows buffer))))
+;(defadvice recompile (around compile/save-window-excursion first () activate)
+;  (save-window-excursion ad-do-it))
 
-(global-set-key (kbd "<f5>") 'compile)
+(defun recompile-after-save()
+  (message "Compiling after saving...")
+;  (interactive)
+;  (ignore-errors (kill-compilation))
+  (recompile)
+  )
+;
+; (add-to-list 'after-save-hook 'recompile-after-save)
+
+
+(global-set-key (kbd "<f5>") 'recompile)
+
+(global-set-key (kbd "C-<f5>") (
+   lambda()
+	 (interactive)
+	 (display-buffer "*compilation*")
+	 (run-at-time "2 sec" nil 'no-color-modeline)))
