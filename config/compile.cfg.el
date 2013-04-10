fad82dd6b605f528147a389045cff23249ad68b2
David Villa Alises <David.Villa@gmail.com>
2013-04-10 11:41:22 +0200

    new config, remember buffer with compile command

--- config/compile.cfg.el
+++ config/compile.cfg.el
@@ -1,5 +1,4 @@
 ; -*- mode:lisp -*-
-; http://vwood.github.com/emacs-compile-on-save.html
 
 ;| Convenience configuration for ``compile`` command.
 ;|
@@ -18,23 +17,39 @@
 ;|
 ;| (new in version 0.20130327)
 
+; References:
+; - http://vwood.github.com/emacs-compile-on-save.html
+; - http://comments.gmane.org/gmane.emacs.devel/156498
+
 (setq compilation-scroll-output 'first-error)
 
 ; automatically save buffer when ask for compilation
 ; http://stackoverflow.com/questions/2062492/save-and-compile-automatically
 (setq compilation-ask-about-save nil)
 
+(setq modeline-timer nil)
 
 ; http://lazywithclass.posterous.com/emacs-functions-to-ease-tdd
-(defun color-modeline(color)
-  "Colors the modeline, green success red failure"
+(defun modeline-set-color (color)
+  "Colors the modeline"
   (interactive)
   (set-face-background 'modeline color))
 
-(defun no-color-modeline()
-  "No color for the modeline"
-  (interactive)
-  (set-face-background 'modeline nil))
+(defun modeline-cancel-timer ()
+  (let ((m modeline-timer))
+	(when m
+	  (cancel-timer m)
+	  (setq modeline-timer nil))))
+
+(defun modeline-delayed-clean ()
+  (modeline-cancel-timer)
+  (setq modeline-timer
+		(run-at-time "2 sec" nil 'modeline-set-color nil)))
+
+; (defun no-color-modeline()
+;   "No color for the modeline"
+;   (interactive)
+;   (set-face-background 'modeline nil))
 
 
 ; from http://emacswiki.org/emacs/CompileCommand (Xu Yuan)
@@ -57,21 +72,26 @@
 (defun open-compilation-buffer()
     (interactive)
     (display-buffer "*compilation*")
-    (run-at-time "2 sec" nil 'no-color-modeline))
+	(modeline-delayed-clean)
+	)
+
 
 (defun compilation-exit-hook (status code msg)
   ;; If M-x compile exists with a 0
   (if (and (eq status 'exit) (zerop code))
     (progn
       (if (string-match "warning:" (buffer-string))
-	  (color-modeline "orange")
-	  (progn (color-modeline "YellowGreen")
-	   (run-at-time "2 sec" nil 'no-color-modeline)))
+		  (modeline-set-color "orange")
+		  (progn (modeline-set-color "YellowGreen")
+				 (modeline-delayed-clean)))
+;				 (run-at-time "2 sec" nil 'modeline-set-color nil)))
       (other-buffer (get-buffer "*compilation*"))
 ;      (delete-windows-on (get-buffer "*compilation*"))
       )
-    (progn (color-modeline "OrangeRed")
-	   (open-compilation-buffer)))
+    (progn
+	  (modeline-set-color "OrangeRed")
+	  (open-compilation-buffer)))
+
   (setq current-frame (car (car (cdr (current-frame-configuration)))))
   (select-frame-set-input-focus current-frame)
   ;; Always return the anticipated result of compilation-exit-message-function
@@ -88,26 +108,55 @@
 
 ;http://vwood.github.com/emacs-compile-on-save.html
 ;; Prevent compilation buffer from showing up
-;(defadvice compile (around compile/save-window-excursion first () activate)
-;  (save-window-excursion ad-do-it))
+
+(defun hide-compilation-buffer
+  (lambda()
+	(defadvice compile (around compile/save-window-excursion first () activate)
+	  (save-window-excursion ad-do-it))
+	))
+
+; (hide-compilation-buffer)
 
 (defadvice recompile (around compile/save-window-excursion first () activate)
-  (save-window-excursion ad-do-it))
+   (save-window-excursion ad-do-it))
 
-(defun recompile-after-save()
-  (message "Compiling after saving...")
-;  (interactive)
-;  (ignore-errors (kill-compilation))
-  (recompile)
-  )
-;
-; (add-to-list 'after-save-hook 'recompile-after-save)
+; (defun recompile-after-save()
+;   (message "Compiling after saving...")
+; ;  (interactive)
+; ;  (ignore-errors (kill-compilation))
+;   (recompile)
+;   )
+; ;
+; ; (add-to-list 'after-save-hook 'recompile-after-save)
 
 
-(global-set-key (kbd "<f5>")
-  (lambda()
-    (interactive)
-    (set-face-background 'modeline "LightBlue")
-    (recompile)))
+; (global-set-key (kbd "<f5>")
+;   (lambda()
+;     (interactive)
+;     (set-face-background 'modeline "LightBlue")
+; 	(recompile)))
 
+(global-set-key (kbd "<f5>")  'compile-again)
 (global-set-key (kbd "C-<f5>") 'open-compilation-buffer)
+
+
+; http://emacswiki.org/emacs/CompileCommand - (Recompiling)
+; (global-set-key [(control c) (c)] 'compile-again)
+
+(setq compilation-last-buffer nil)
+(defun compile-again (pfx)
+  """Run the same compile as the last time.
+
+     If there was no last time, or there is a prefix argument, this acts like
+     M-x compile.
+  """
+ (interactive "p")
+ (save-some-buffers 1)
+ (if compilation-last-buffer
+     (progn
+       (set-buffer compilation-last-buffer)
+	   (modeline-cancel-timer)
+	   (set-face-background 'modeline "LightBlue")
+	   (recompile)
+	   )
+   (call-interactively 'compile)))
