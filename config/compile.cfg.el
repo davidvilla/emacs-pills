5d252a9779437ee9d0b21e7b208c9c9bedf9cc83
David Villa Alises <David.Villa@gmail.com>
2013-04-11 19:03:18 +0200

    doc

--- config/compile.cfg.el
+++ config/compile.cfg.el
@@ -3,8 +3,9 @@
 ;| Convenience configuration for ``compile`` command.
 ;|
 ;| See the screencast:
+;| ::
 ;|
-;| |compile-screencast|_:
+;|    |compile-screencast|_
 ;|
 ;| .. |compile-screencast| image:: http://i4.ytimg.com/vi/34B3mkPj01s/3.jpg?time=1365699591540
 ;| .. _compile-screencast: http://youtu.be/34B3mkPj01s
